8ea78b89debacb26180e74bd83289cd835a64940
Tom Whipple <mail@tomwhipple.com>
2013-06-30 13:46:34 -0700

    play the video

--- trackr.py
+++ trackr.py
@@ -4,10 +4,13 @@ Examine videos & track objects
 """
 
 import sys
+import time
 import argparse
 
 import cv
 import cv2
+
+WIN_NAME = "trackr"
 
 def main(argv=None):
     if argv is None:
@@ -32,12 +35,39 @@ def main(argv=None):
         print "Video not open"
         return
         
-    width = video.get(cv.CV_CAP_PROP_FRAME_WIDTH)
-    height = video.get(cv.CV_CAP_PROP_FRAME_HEIGHT)
+    width = int(video.get(cv.CV_CAP_PROP_FRAME_WIDTH))
+    height = int(video.get(cv.CV_CAP_PROP_FRAME_HEIGHT))
     fps = video.get(cv.CV_CAP_PROP_FPS)
     
-    print "opened {w}x{h} video @ {f} fps".format(w=width,h=height,f=fps)
+    print "opened {w}x{h} video @ {f}fps".format(w=width,h=height,f=fps)
     
+    cv2.namedWindow(WIN_NAME)
+    cv2.moveWindow(WIN_NAME, 0, 0)
+    cv2.resizeWindow(WIN_NAME, width, height)
+    
+    frame_count = 0
+    while video.grab():
+        got_frame, frame = video.retrieve()
+        if not got_frame:
+            print "frame miss"
+            continue
+        
+        frame_count += 1
+        print "frame: {c}   \r".format(c=frame_count), 
+        sys.stdout.flush()
+        
+        cv2.imshow(WIN_NAME, frame)
+        # time.sleep(0.01)
+        key = cv2.waitKey(int(1000.0/fps))
+        if key == 27:
+            return
+        elif key >= 0:
+            print "\nkey: {k}\n".format(k=key)
+        
+    print
+    
+    # cv2.waitKey()
+    video.release()
     
 if __name__ == '__main__':
     sys.exit(main())
