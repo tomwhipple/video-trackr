5902be3c3824f1578f30d7481271e1b2ef200a5f
Andrew Chen <andrew@kavalry.co>
2013-09-29 16:35:35 -0700

    switch to cv2 syntax

--- trackr.py
+++ trackr.py
@@ -7,7 +7,6 @@ import sys
 import time
 import argparse
 
-import cv
 import cv2
 import numpy as np
 
@@ -19,7 +18,7 @@ MAX_TIME_DELTA = 0.25
 MIN_TIME_DELTA = 0.05
 
 GREEN = (0, 255, 0)
-FONT = cv.CV_FONT_HERSHEY_PLAIN
+FONT = cv2.FONT_HERSHEY_PLAIN
 
 def make_nth_named_window(name, height, n=0, x=0):
     cv2.namedWindow(name)
@@ -56,9 +55,9 @@ def main(argv=None):
         print "Video not open"
         return
         
-    width = int(video.get(cv.CV_CAP_PROP_FRAME_WIDTH))
-    height = int(video.get(cv.CV_CAP_PROP_FRAME_HEIGHT))
-    fps = video.get(cv.CV_CAP_PROP_FPS)
+    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = video.get(cv2.CAP_PROP_FPS)
     
     print "opened {w}x{h} video @ {f}fps".format(w=width,h=height,f=fps)
     
